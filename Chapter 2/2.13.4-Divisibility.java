6eac0b4fe57067da6bd362368595566c62b80812
RElMAGINED <strawwberrycow@gmail.com>
2024-11-15 13:45:14 -0500

    Update 2.13.4-Divisibility.java

--- Chapter 2/2.13.4-Divisibility.java
+++ Chapter 2/2.13.4-Divisibility.java
@@ -5,7 +5,6 @@ public class Divisibility extends ConsoleProgram
         int dividend = readInt("Enter the dividend: ");
         int divisor = readInt("Enter the divisor: ");
         
-        
         if(divisor != 0 && dividend / divisor == (double) dividend / divisor)
         {
             System.out.println(dividend + " is divisible by " + divisor + "!");
